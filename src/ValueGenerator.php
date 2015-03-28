a319012b82350e3bdebbf6291e1d210381525a73
Mariano Gappa <mariano@movio.co>
2015-03-29 11:33:15 +1300

    Added value generator for integer types considering ranges but disregarding nulls for now.

--- src/ValueGenerator.php
+++ src/ValueGenerator.php
@@ -33,18 +33,27 @@ class DatetimeValueGenerator extends ValueGenerator {
 }
 
 class IntValueGenerator extends ValueGenerator {
+    private static $ranges = [
+        'tinyint'   => [ false => [ 'min' => -128,                 'max' => 127                 ], true => [ 'min' => 0, 'max' => 255 ] ],
+        'smallint'  => [ false => [ 'min' => -32768,               'max' => 32767               ], true => [ 'min' => 0, 'max' => 65535 ] ],
+        'int'       => [ false => [ 'min' => -2147483648,          'max' => 2147483647          ], true => [ 'min' => 0, 'max' => 4294967295 ] ],
+        'mediumint' => [ false => [ 'min' => -8388608,             'max' => 8388607             ], true => [ 'min' => 0, 'max' => 16777215 ] ],
+        'bigint'    => [ false => [ 'min' => -9223372036854775808, 'max' => 9223372036854775807 ], true => [ 'min' => 0, 'max' => 18446744073709551615 ] ]
+    ];
+	
     public function next() {
-        return rand();
+        $ranges = self::$ranges[$this->columnStructure->dataType][$this->columnStructure->unsigned];
+        return rand($ranges['min'], $ranges['max']);
     }
 
     public static function isFitGenerator($columnStructure) {
-        return in_array($columnStructure->dataType, ['bigint', 'int']);
+        return in_array($columnStructure->dataType, array_keys(self::$ranges));
     }
 }
 
 class EnumValueGenerator extends ValueGenerator {
 	public function next() {
-		return $this->columnStructure->values[array_rand($this->columnStructure->values)];
+	 	return $this->columnStructure->values[array_rand($this->columnStructure->values)];
 	}
 
 	public static function isFitGenerator($columnStructure) {
